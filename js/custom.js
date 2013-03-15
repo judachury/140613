1cab2714e4170c4f6299a37d5486975f408d010f
unknown <Tom@Tom-PC.(none)>
2013-03-15 00:22:35 +0000

    Added CSS for client non-functional prototype

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,12 @@
-//Custom Javascript File
+//Custom Javascript File for BUILDING A CALCULATOR
 
-
+/*
+Write some javascript in your normal style to and the follwing functionality.
+1. 	When clicking an operand button (number) update the display with that buttons value.
+2. 	When clicking another operand update the display with that buttons value and the previous value. 
+3. 	When clicking the clear button clear the display and reset it to 0.
+4. 	When clicking the backspace button update the display value removing the last digit,
+	Repeatedly cliking the backspace button will remove another single digit at a time.
+*/
 
 
